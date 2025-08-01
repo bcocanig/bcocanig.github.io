3f4d6e2820c044ebe3c8ac38f62be51fa231c1e0
Brandon C <bgpkr@att.net>
2025-08-01 10:30:36 -0500

    react site upload

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,11 @@
+import HotScottyWebsite from './HotScottyWebsite.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <HotScottyWebsite />
+    </div>
+  );
+}
+
+export default App;
